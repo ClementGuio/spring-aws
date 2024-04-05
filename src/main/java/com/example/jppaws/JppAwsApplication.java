bec974667f3b3e3a0590963698dad14c2f2f8075
ClementGuio <clementguio@gmail.com>
2024-04-05 16:15:27 +0200

    first commit

---
+++ src/main/java/com/example/jppaws/JppAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jppaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JppAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JppAwsApplication.class, args);
+	}
+
+}
